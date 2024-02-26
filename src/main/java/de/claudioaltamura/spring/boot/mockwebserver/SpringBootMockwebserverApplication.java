d4073f10fa4e9228fe5bfffca21058f884da559a
Claudio Altamura <claudio@claudioaltamura.de>
2024-02-27 00:34:59 +0100

    chore: spotless apply

--- src/main/java/de/claudioaltamura/spring/boot/mockwebserver/SpringBootMockwebserverApplication.java
+++ src/main/java/de/claudioaltamura/spring/boot/mockwebserver/SpringBootMockwebserverApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringBootMockwebserverApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootMockwebserverApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(SpringBootMockwebserverApplication.class, args);
+  }
 }
